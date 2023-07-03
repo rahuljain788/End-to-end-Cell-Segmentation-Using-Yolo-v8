889ba02da48eb5c6adfc3d546c0e8e59e6f1e973
Rahul Jain <rahuljain809@gmail.com>
2023-07-03 09:59:30 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Rahul Jain',
+    author_email= 'rahuljain809@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
